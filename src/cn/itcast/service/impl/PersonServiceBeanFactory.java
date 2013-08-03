e084ed418a001477e49ffca42364f4513443b27b
Free <li5220008@163.com>
2013-08-03 12:26:15 +0800

    Signed-off-by: Free <li5220008@163.com>

--- src/cn/itcast/service/impl/PersonServiceBeanFactory.java
+++ src/cn/itcast/service/impl/PersonServiceBeanFactory.java
@@ -3,5 +3,8 @@ package cn.itcast.service.impl;
 public class PersonServiceBeanFactory {
 	public static PersonServiceBean createPersonServiceBean(){
 		return new PersonServiceBean();
-	}
+	} 	
+	public PersonServiceBean createPersonServiceBean2(){
+		return new PersonServiceBean();
+	} 	
 }
