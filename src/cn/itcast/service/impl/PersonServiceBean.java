7a854390360e96095cda142cb56132b82458bf17
Free <li5220008@163.com>
2013-08-06 18:28:47 +0800

    Change-Id: Iefebdd3ea8f651c2ecdfb3b76cdd365bcad54ee2

--- src/cn/itcast/service/impl/PersonServiceBean.java
+++ src/cn/itcast/service/impl/PersonServiceBean.java
@@ -2,21 +2,35 @@ package cn.itcast.service.impl;
 
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
+import org.aspectj.lang.annotation.After;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 
 import cn.itcast.dao.PersonDao;
 import cn.itcast.service.PersonService;
 
-@Service
+@Service("personService") 
+@Scope("prototype")
 public class PersonServiceBean implements PersonService {
 	//@Autowired
 	private PersonDao personDao;
 	
 	public PersonServiceBean(){}
+	@PostConstruct
+	public void init(){
+		System.out.println("初始化方法！");
+	}
+	
+	@PreDestroy
+	public void distroy(){
+		System.out.println("销毁关闭资源。");
+	}
 	
 	public void setPersonDao(PersonDao personDao) {
 		this.personDao = personDao;
